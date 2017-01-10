0cf4467a268563ea97e8de43812f55bea15f93ee
Jon Gilkison <jon.gilkison@gmail.com>
2017-01-11 00:05:14 +0700

    Fix for autoload

--- ilab-publish-tool.php
+++ ilab-publish-tool.php
@@ -8,11 +8,12 @@ Version: 0.1.0
 Author URI: http://interfacelab.com
 */
 
-
-require_once('vendor/autoload.php');
-
 define('ILAB_PUBLISH_TOOLS_DIR',dirname(__FILE__));
 define('ILAB_PUBLISH_VIEW_DIR',ILAB_PUBLISH_TOOLS_DIR.'/views');
+
+if (file_exists(ILAB_PUBLISH_TOOLS_DIR.'/vendor/autoload.php')) {
+	require_once('vendor/autoload.php');
+}
 
 require_once('helpers/ilab-publish-view.php');
 require_once('classes/ilab-publish-tool-plugin.php');
